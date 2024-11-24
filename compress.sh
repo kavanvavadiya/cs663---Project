1477233c55118c4bb77ce6251066dce3700201bc
Kavan <kavanvavadiya24@gmail.com>
2024-11-24 16:26:59 +0530

    minor changes

--- compress.sh
+++ compress.sh
@@ -1,6 +1,6 @@
 rm -r compressed
 mkdir compressed
 for i in {1..5};do pbmtojbg result/m_im$i.pbm compressed/m_im$i.jbg ;done
-for i in {1..5};do zpaq pa compressed/im$i.archive  compressed/m_im$i.jbg result/res_im$i.png ;done
+for i in {1..5};do zpaq a compressed/im$i.archive  compressed/m_im$i.jbg result/res_im$i.png ;done
 
 
